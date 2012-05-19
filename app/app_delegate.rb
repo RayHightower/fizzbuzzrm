426375baecafcc67ce8c125d6aa1356f00c75310
RayHightower <rth@wisdomgroup.com>
2012-05-19 01:26:25 -0500

    our first passing test

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,12 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    alert = UIAlertView.new
+    alert.message = "Hey Chicago Code Camp!"
+    alert.show
     true
   end
 end
 
+# Now testing Guard with fizzbuzz. Let's go! ~RTH
+# Now testing Guard with fizzbuzz. Let's go! ~RTH
 # Now testing Guard with fizzbuzz. Let's go! ~RTH
