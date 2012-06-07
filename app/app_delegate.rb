bc39f80327dbd705325f8e3abb2f8288da286384
RayHightower <rth@wisdomgroup.com>
2012-06-07 00:01:05 -0500

    keep the commits up to date...

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    alert = UIAlertView.new
+    alert.message = "Hey Chicago Code Camp!"
+    alert.show
     true
   end
 end
