c854faf6fb06ca36eb4e9d391fe4816147a81b06
RayHightower <rth@wisdomgroup.com>
2012-06-08 10:57:27 -0500

    fun with view controllers

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,12 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    alert = UIAlertView.new
-    alert.message = "Let's do FizzBuzz!"
-    alert.show
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = NSBundle.mainBundle.loadNibNamed('FizzBuzzView', owner:self, options:nil).first
+    @window.rootViewController.wantsFullScreenLayout = true
+    @window.makeKeyAndVisible
+#    alert = UIAlertView.new
+#    alert.message = "Let's do FizzBuzz!"
+#    alert.show
     true
   end
 end
