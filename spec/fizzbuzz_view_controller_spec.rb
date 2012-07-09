14a453d7c052e11a087e9ece26283301d752242b
RayHightower <rth@wisdomgroup.com>
2012-07-09 12:22:15 -0500

    after upgrade to v 1.15

--- spec/fizzbuzz_view_controller_spec.rb
+++ spec/fizzbuzz_view_controller_spec.rb
@@ -8,29 +8,29 @@ describe "Calculator Function" do
   end
 
   it 'should return the fizz for 3' do
-    FizzBuzzViewController.calc(8).should == 'fizz'
+    FizzBuzzViewController.calc(3).should == 'fizz'
   end
 
   it 'should return the buzz for 5' do
     FizzBuzzViewController.calc(5).should == 'buzz'
   end
 
-  (1..100).each do |another_number|
-    if another_number % 15 == 0
+  (1..100).each do |this_number|
+    if this_number % 15 == 0
       it 'should return buzz for multiples of 15' do
-         FizzBuzzViewController.calc(another_number).should == 'fizzbuzz'
+         FizzBuzzViewController.calc(this_number).should == 'fizzbuzz'
       end
-    elsif another_number % 3 == 0
+    elsif this_number % 3 == 0
       it 'should return fizz for multiples of 3' do
-         FizzBuzzViewController.calc(another_number).should == 'fizz'
+         FizzBuzzViewController.calc(this_number).should == 'fizz'
       end
-    elsif another_number % 5 == 0
+    elsif this_number % 5 == 0
       it 'should return buzz for multiples of 5' do
-         FizzBuzzViewController.calc(another_number).should == 'buzz'
+         FizzBuzzViewController.calc(this_number).should == 'buzz'
       end
     else
       it 'should return the original number' do
-        FizzBuzzViewController.calc(another_number).should == another_number
+        FizzBuzzViewController.calc(this_number).should == this_number
       end
    end
   end
