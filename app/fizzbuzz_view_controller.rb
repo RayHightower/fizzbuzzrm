7a3f8bf26a938da4cf7cd65b81178799d3c2f15e
Raymond T. Hightower <rth@wisdomgroup.com>
2012-08-14 17:56:59 -0500

    prep for cocoaheads

--- app/fizzbuzz_view_controller.rb
+++ app/fizzbuzz_view_controller.rb
@@ -12,7 +12,7 @@ class FizzBuzzViewController < UIViewController
 
   def self.calc(this_number)
     return 'fizzbuzz' if this_number % 15 == 0
-    return 'buzzard' if this_number % 5 == 0
+    return 'buzz' if this_number % 5 == 0
     return 'fizz' if this_number % 3 == 0
     this_number
     
