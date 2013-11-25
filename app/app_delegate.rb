33785eaf81341622ebd8055221859466a7751f69
Raymond T. Hightower <rth@wisdomgroup.com>
2013-11-25 13:56:39 -0600

    yes, @window is suppoded to be an instance variable!

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,11 +2,11 @@ class AppDelegate
 
    def application(application, didFinishLaunchingWithOptions:launchOptions)
 
-     window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
-     window.rootViewController = FizzBuzzViewController.alloc.init
+     @window.rootViewController = FizzBuzzViewController.alloc.init
 
-     window.makeKeyAndVisible
+     @window.makeKeyAndVisible
 
      true
 
